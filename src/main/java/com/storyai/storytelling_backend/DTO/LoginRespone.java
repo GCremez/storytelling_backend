1c08349e65e2ffd34e200674f9be2c784af97cbe
GCremez <salauolorunjuedalo@gmail.com>
2025-11-25 17:49:51 +0200

    FIXING LOGIN RESPONSE

--- src/main/java/com/storyai/storytelling_backend/DTO/LoginRespone.java
+++ src/main/java/com/storyai/storytelling_backend/DTO/LoginRespone.java
@@ -37,5 +37,30 @@ public class LoginRespone {
     public Boolean getIsVerified() { return isVerified; }
     public void setIsVerified(Boolean isVerified) { this.isVerified = isVerified; }
   }
+
+  public LoginRespone() {}
+
+  public LoginRespone(String accessToken, String refreshToken, long expiresIn, User user) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+    this.expiresIn = expiresIn;
+    this.user = user;
+  }
+
+  // GETTERS AND SETTERS
+  public String getAccessToken() { return accessToken; }
+  public void setAccessToken(String accessToken) { this.accessToken = accessToken; }
+
+  public String getRefreshToken() { return refreshToken; }
+  public void setRefreshToken(String refreshToken) { this.refreshToken = refreshToken; }
+
+  public String getTokenType() { return tokenType; }
+  public void setTokenType(String tokenType) { this.tokenType = tokenType; }
+
+  public long getExpiresIn() { return expiresIn; }
+  public void setExpiresIn(long expiresIn) { this.expiresIn = expiresIn; }
+
+  public User getUser() { return user; }
+  public void setUser(User user) { this.user = user; }
 }
 
