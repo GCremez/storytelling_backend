04f9b37d578c8f11e2a1d39378639b3bee1f4511
GCremez <salauolorunjuedalo@gmail.com>
2025-11-26 14:50:52 +0200

    TokenException

--- src/main/java/com/storyai/storytelling_backend/exception/TokenException.java
+++ src/main/java/com/storyai/storytelling_backend/exception/TokenException.java
@@ -1,4 +1,19 @@
 package com.storyai.storytelling_backend.exception;
 
-public class TokenException {
+public class TokenException extends AuthException {
+  public TokenException(String message, String errorCode) {
+    super(message, errorCode);
+  }
+
+  public static TokenException expired() {
+    return new TokenException("Token has expired", "TOKEN_EXPIRED");
+  }
+
+  public static TokenException invalid() {
+    return new TokenException("Invalid token", "TOKEN_INVALID");
+  }
+
+  public static TokenException refreshInvalid() {
+    return new TokenException("Invalid refresh token", "REFRESH_TOKEN_INVALID");
+  }
 }
