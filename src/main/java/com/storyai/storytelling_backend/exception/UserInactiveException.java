84653ded623f5f604aed129e3f9dae07798ed0b2
GCremez <salauolorunjuedalo@gmail.com>
2025-11-26 14:54:20 +0200

    UserInactiveException

--- src/main/java/com/storyai/storytelling_backend/exception/UserInactiveException.java
+++ src/main/java/com/storyai/storytelling_backend/exception/UserInactiveException.java
@@ -1,4 +1,7 @@
 package com.storyai.storytelling_backend.exception;
 
-public class UserInactiveException {
+public class UserInactiveException extends AuthException {
+  public UserInactiveException() {
+    super("Your account has been deactivated. Please contact support.", "USER_INACTIVE");
+  }
 }
