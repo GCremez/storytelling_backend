1e6e4849184a4654b32bd93acdce9e0f72b10a0e
GCremez <salauolorunjuedalo@gmail.com>
2025-11-26 14:48:39 +0200

    RateLimitException

--- src/main/java/com/storyai/storytelling_backend/exception/RateLimitException.java
+++ src/main/java/com/storyai/storytelling_backend/exception/RateLimitException.java
@@ -1,4 +1,14 @@
 package com.storyai.storytelling_backend.exception;
 
-public class RateLimitException {
+public class RateLimitException extends AuthException {
+  private final int retryAfterSeconds;
+
+  public RateLimitException(String message, int retryAfterSeconds) {
+    super(message, "RATE_LIMIT_EXCEEDED");
+    this.retryAfterSeconds = retryAfterSeconds;
+  }
+
+  public int getRetryAfterSeconds() {
+    return retryAfterSeconds;
+  }
 }
