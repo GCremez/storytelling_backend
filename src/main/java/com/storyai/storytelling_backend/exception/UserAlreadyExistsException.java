1a94add8f4cdde0aa7db11234b535124ea74e1fe
GCremez <salauolorunjuedalo@gmail.com>
2025-11-26 14:52:56 +0200

    UserAlreadyExistsException

--- src/main/java/com/storyai/storytelling_backend/exception/UserAlreadyExistsException.java
+++ src/main/java/com/storyai/storytelling_backend/exception/UserAlreadyExistsException.java
@@ -1,4 +1,21 @@
 package com.storyai.storytelling_backend.exception;
 
-public class UserAlreadyExistsException {
+public class UserAlreadyExistsException extends AuthException {
+  private final String field;
+  private final String value;
+
+  public UserAlreadyExistsException(String field, String value) {
+    super(field + " already exists: " + value,
+      field.equals("username") ? "USERNAME_TAKEN" : "EMAIL_TAKEN");
+    this.field = field;
+    this.value = value;
+  }
+
+  public String getField() {
+    return field;
+  }
+
+  public String getValue() {
+    return value;
+  }
 }
