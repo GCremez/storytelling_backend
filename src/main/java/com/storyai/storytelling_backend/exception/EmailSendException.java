632244e9329b9f18aaafc29c384deaba1d399960
GCremez <salauolorunjuedalo@gmail.com>
2025-11-26 14:38:49 +0200

    EmailSendException

--- src/main/java/com/storyai/storytelling_backend/exception/EmailSendException.java
+++ src/main/java/com/storyai/storytelling_backend/exception/EmailSendException.java
@@ -1,4 +1,12 @@
 package com.storyai.storytelling_backend.exception;
 
-public class EmailSendException {
+public class EmailSendException extends AuthException{
+  public EmailSendException(String message) {
+    super("Failed to send email: " + message, "EMAIL_SEND_FAILED");
+  }
+
+  public EmailSendException(String message, Throwable cause) {
+    super("Failed to send email: " + message, "EMAIL_SEND_FAILED");
+    initCause(cause);
+  }
 }
