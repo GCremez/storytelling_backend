a0551352942155cd83c51e8238df076a22da662a
GCremez <salauolorunjuedalo@gmail.com>
2025-12-04 14:11:24 +0200

    JwtService

--- src/main/java/com/storyai/storytelling_backend/service/jwtService.java
+++ src/main/java/com/storyai/storytelling_backend/service/jwtService.java
@@ -1,3 +1,22 @@
 package com.storyai.storytelling_backend.service;
 
-public class jwtService {}
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class jwtService {
+
+  private static final Logger logger = LoggerFactory.getLogger(jwtService.class);
+
+  @Value("${jwt.secret}")
+  private String secreat;
+
+  @Value("${jwt.expiration:900000}")
+  private Long expiration;
+
+  @Value("${jwt.refresh.expiration:604800000}") // 7 days default
+  private Long refreshExpiration;
+}
