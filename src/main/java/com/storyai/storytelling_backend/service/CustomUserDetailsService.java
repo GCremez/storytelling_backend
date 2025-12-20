18a6373b08c05bca6b5df40ac45a28af5fb0821d
GCremez <salauolorunjuedalo@gmail.com>
2025-12-20 19:22:12 +0200

    Update the customUserDetailsService

--- src/main/java/com/storyai/storytelling_backend/service/CustomUserDetailsService.java
+++ src/main/java/com/storyai/storytelling_backend/service/CustomUserDetailsService.java
@@ -1,4 +1,57 @@
 package com.storyai.storytelling_backend.service;
 
-public class CustomUserDetailsService {
+import com.storyai.storytelling_backend.entity.User;
+import com.storyai.storytelling_backend.repository.UserRepository;
+import com.storyai.storytelling_backend.security.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public CustomUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username)
+    throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+      .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+    return new CustomUserDetails(user);
+  }
+
+  /**
+   * Load User by email ( For login with email)
+   */
+  @Transactional(readOnly = true)
+  public UserDetails loadUserByEmail(String email)
+    throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(email)
+      .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+    return new CustomUserDetails(user);
+  }
+
+  /**
+   * Load user by username or email
+   * @param usernameOrEmail
+   * @return
+   * @throws UsernameNotFoundException
+   */
+  @Transactional(readOnly = true)
+  public UserDetails loadUserByUsernameOrEmail(String usernameOrEmail)
+    throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(usernameOrEmail)
+      .orElseThrow(() -> new UsernameNotFoundException("User not found: " + usernameOrEmail));
+
+    return new CustomUserDetails(user);
+  }
 }
