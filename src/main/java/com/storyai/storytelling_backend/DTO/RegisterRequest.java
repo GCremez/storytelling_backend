164aa92d4b37012026cd837152b2c32127eefecc
GCremez <salauolorunjuedalo@gmail.com>
2026-04-03 16:43:27 +0300

    Fixed Password Validation Issues

--- src/main/java/com/storyai/storytelling_backend/DTO/RegisterRequest.java
+++ src/main/java/com/storyai/storytelling_backend/DTO/RegisterRequest.java
@@ -17,7 +17,7 @@ public class RegisterRequest {
   @NotBlank(message = "Password is required")
   @Size(min = 8, message = "Password must be at least 6 characters long")
   @Pattern(
-      regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]+$",
+      regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
       message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
   private String password;
 
