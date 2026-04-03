1d233113610c52d7533aa41e56744d40aeb7a613
GCremez <salauolorunjuedalo@gmail.com>
2026-04-03 16:38:45 +0300

    Update JwtUtil to extract username

--- src/main/java/com/storyai/storytelling_backend/config/JwtUtil.java
+++ src/main/java/com/storyai/storytelling_backend/config/JwtUtil.java
@@ -8,6 +8,7 @@ import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -31,5 +32,30 @@ public class JwtUtil {
         .expiration(Date.from(Instant.now().plus(expiration, ChronoUnit.SECONDS)))
         .signWith(getSigningKey())
         .compact();
+  }
+
+  public String extractUsername(String token) {
+    return getClaims(token).getSubject();
+  }
+
+  public Date extractExpiration(String token) {
+    return getClaims(token).getExpiration();
+  }
+
+  private Claims getClaims(String token) {
+    return Jwts.parser()
+        .verifyWith(getSigningKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  public Boolean validateToken(String token, String username) {
+    final String extractedUsername = extractUsername(token);
+    return (extractedUsername.equals(username) && !isTokenExpired(token));
+  }
+
+  private Boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
   }
 }
